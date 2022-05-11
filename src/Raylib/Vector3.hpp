3d8f4d62603c69ac23b214bce411badb91343ded
aurelenc <aurele.nicolas@epitech.eu>
2022-05-11 16:05:47 +0200

    Documented Vector3 class

--- src/Raylib/Vector3.hpp
+++ src/Raylib/Vector3.hpp
@@ -12,24 +12,64 @@ namespace raylib {
 
 class Vector3 {
     public:
+        /// @brief 3D constructor of the vector
+        /// @param x the X axis value of the vector
+        /// @param y the Y axis value of the vector
+        /// @param z the Z axis value of the vector
         Vector3(float x, float y, float z);
+        /// @brief 2D constructor of the vector (defaults Z to 0)
+        /// @param x the X axis value of the vector
+        /// @param y the Y axis value of the vector
         Vector3(float x, float y);
-        ~Vector3();
-        float getX() const { return _x; }
-        void setX(float x) { _x = x; }
-        float getY() const { return _y; }
-        void setY(float y) { _y = y; }
-        float getZ() const { return _z; }
-        void setZ(float z) { _z = z; }
-        Vector3 &operator=(const Vector3& vector);
+        /// @brief The copy constructor
+        /// @param vector the vector to copy
         Vector3(const Vector3& vector);
+        /// @brief Destructor
+        ~Vector3();
+        /// @brief Get the X axis value of the vector
+        /// @return the X axis value of the vector
+        float getX() const { return _x; }
+        /// @brief Set the X acis value of the vector
+        /// @param x the new X axis value of the vector
+        void setX(float x) { _x = x; }
+        /// @brief Get the Y axis value of the vector
+        /// @return the Y axis value of the vector
+        float getY() const { return _y; }
+        /// @brief Set the Y acis value of the vector
+        /// @param y the new Y axis value of the vector
+        void setY(float y) { _y = y; }
+        /// @brief Get the Z axis value of the vector
+        /// @return the Z axis value of the vector
+        float getZ() const { return _z; }
+        /// @brief Set the Z acis value of the vector
+        /// @param z the new Z axis value of the vector
+        void setZ(float z) { _z = z; }
+        /// @brief The Vector3 copy operator
+        /// @param vector the Vector3 to copy
+        Vector3 &operator=(const Vector3& vector);
+        /// @brief The Vector3 add operator
+        /// @param vector the Vector3 to add
         Vector3 operator+(const Vector3& vector);
+        /// @brief The Vector3 assignment add operator
+        /// @param vector the Vector3 to assignment add
         Vector3 &operator+=(const Vector3& vector);
+        /// @brief The Vector3 remove operator
+        /// @param vector the Vector3 to remove
         Vector3 operator-(const Vector3& vector);
+        /// @brief The Vector3 assignment remove operator
+        /// @param vector the Vector3 to assignment remove
         Vector3 &operator-=(const Vector3& vector);
+        /// @brief The Vector3 multiply operator
+        /// @param vector the Vector3 to multiply
         Vector3 operator*(const Vector3& vector);
+        /// @brief The Vector3 assignment multiply operator
+        /// @param vector the Vector3 to assignment multiply
         Vector3 &operator*=(const Vector3& vector);
+        /// @brief The Vector3 divide operator
+        /// @param vector the Vector3 to divide
         Vector3 operator/(const Vector3& vector);
+        /// @brief The Vector3 assignment divide operator
+        /// @param vector the Vector3 to assignment divide
         Vector3 &operator/=(const Vector3& vector);
 
     private:
