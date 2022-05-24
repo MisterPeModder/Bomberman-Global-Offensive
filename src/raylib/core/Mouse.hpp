0af9543e778069a0dcf0d0c70c0dc0c31ba4f1d1
aurelenc <aurele.nicolas@epitech.eu>
2022-05-24 19:25:32 +0200

    Remove bad references in Mouse.cpp

--- src/raylib/core/Mouse.hpp
+++ src/raylib/core/Mouse.hpp
@@ -27,54 +27,54 @@ namespace raylib
             /// @param button the button to check
             ///
             /// @return true if the button is pressed or false otherwise
-            static constexpr bool isButtonPressed(int &button) { return IsMouseButtonPressed(button); }
+            static constexpr bool isButtonPressed(int button) { return IsMouseButtonPressed(button); }
 
             /// Gets if the button specified in argument is currently being pressed or not
             ///
             /// @param button the button to check
             ///
             /// @return true if the button is pressed or false otherwise
-            static constexpr bool isButtonDown(int &button) { return IsMouseButtonDown(button); }
+            static constexpr bool isButtonDown(int button) { return IsMouseButtonDown(button); }
 
             /// Gets if the button specified in argument has been released once or not
             ///
             /// @param button the button to check
             ///
             /// @return true if the button is pressed or false otherwise
-            static constexpr bool isButtonReleased(int &button) { return IsMouseButtonReleased(button); }
+            static constexpr bool isButtonReleased(int button) { return IsMouseButtonReleased(button); }
 
             /// Gets if the button specified in argument is currently not being pressed or not
             ///
             /// @param button the button to check
             ///
             /// @return true if the button is not being pressed or false otherwise
-            static constexpr bool isButtonUp(int &button) { return IsMouseButtonUp(button); }
+            static constexpr bool isButtonUp(int button) { return IsMouseButtonUp(button); }
 
             /// Gets the mouse's x position.
             ///
             /// @return The mouse's x position.
-            static constexpr int getX(void) { return GetMouseX(); }
+            static constexpr int getX() { return GetMouseX(); }
 
             /// Gets the mouse's y position.
             ///
             /// @return The mouse's y position.
-            static constexpr int getY(void) { return GetMouseY(); }
+            static constexpr int getY() { return GetMouseY(); }
 
             /// Gets the mouse position.
             ///
             /// @return The mouse position.
-            static inline Vector2 getPosition(void) { return GetMousePosition(); }
+            static inline Vector2 getPosition() { return GetMousePosition(); }
 
             /// Gets the mouse delta.
             ///
             /// @return The mouse delta.
-            static inline Vector2 getDelta(void) { return GetMouseDelta(); }
+            static inline Vector2 getDelta() { return GetMouseDelta(); }
 
             /// Sets the mouse position
             ///
             /// @param x The new x position of the mouse
             /// @param y The new y position of the mouse
-            static constexpr void setPosition(float &x, float &y) { SetMousePosition(static_cast<int>(x), static_cast<int>(y)); }
+            static constexpr void setPosition(float x, float y) { SetMousePosition(static_cast<int>(x), static_cast<int>(y)); }
 
             /// Sets the mouse position
             ///
@@ -86,7 +86,7 @@ namespace raylib
             ///
             /// @param offsetX The X offset of the mouse.
             /// @param offsetY The Y offset of the mouse.
-            static constexpr void setOffset(float &offsetX, float &offsetY)  { SetMouseOffset(static_cast<int>(offsetX), static_cast<int>(offsetY)); }
+            static constexpr void setOffset(float offsetX, float offsetY)  { SetMouseOffset(static_cast<int>(offsetX), static_cast<int>(offsetY)); }
             /// Sets the mouse offset
             ///
             /// @param offsetX The X offset of the mouse.
@@ -102,7 +102,7 @@ namespace raylib
             /// Gets the mouse wheel Y movement.
             ///
             /// @return The mouse wheel movement.
-            static constexpr float getWheelMove(void) { return GetMouseWheelMove(); }
+            static constexpr float getWheelMove() { return GetMouseWheelMove(); }
 
             /// Sets the mouse cursor
             ///
