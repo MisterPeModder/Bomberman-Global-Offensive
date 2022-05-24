f5ab40af57c8352103e97c6fffd03bd043592d6a
aurelenc <aurele.nicolas@epitech.eu>
2022-05-24 17:57:14 +0200

    Fix the code formatting of the 'Color' class

--- src/raylib/core/Color.hpp
+++ src/raylib/core/Color.hpp
@@ -26,58 +26,62 @@ namespace raylib
                 /// @param g the amount of green
                 /// @param b the amount of blue
                 /// @param a the alpha
-                Color(unsigned char r = 255, unsigned char g = 255, unsigned char b = 255, unsigned char a = 255);
+                constexpr Color(unsigned char r = 255, unsigned char g = 255, unsigned char b = 255, unsigned char a = 255)
+                {
+                    _color.r = r;
+                    _color.g = g;
+                    _color.b = b;
+                    _color.a = a;
+                }
 
                 /// Destructor
-                ~Color();
+                constexpr ~Color() {}
 
                 /// Get the red amount in the color
                 ///
                 /// @return The red amount
-                unsigned char getRed() const { return _color.r; }
+                constexpr unsigned char getRed() const { return _color.r; }
 
                 /// Set the red amount in the color
                 ///
                 /// @param r The red amount
-                void setRed(unsigned char r) { _color.r = r; }
+                constexpr void setRed(unsigned char r) { _color.r = r; }
 
                 /// Get the green amount in the color
                 ///
                 /// @return The green amount
-                unsigned char getGreen() const { return _color.g; }
+                constexpr unsigned char getGreen() const { return _color.g; }
 
                 /// Set the green amount in the color
                 ///
                 /// @param g The green amount
-                void setGreen(unsigned char g) { _color.g = g; }
+                constexpr void setGreen(unsigned char g) { _color.g = g; }
 
                 /// Get the blue amount in the color
                 ///
                 /// @return The blue amount
-                unsigned char getBlue() const { return _color.b; }
+                constexpr unsigned char getBlue() const { return _color.b; }
 
                 /// Set the blue amount in the color
                 ///
                 /// @param b The blue amount
-                void setBlue(unsigned char b) { _color.b = b; }
+                constexpr void setBlue(unsigned char b) { _color.b = b; }
 
                 /// Get the alpha of the color
                 ///
                 /// @return The alpha
-                unsigned char getAlpha() const { return _color.a; }
+                constexpr unsigned char getAlpha() const { return _color.a; }
 
                 /// Set the alpha of the color
                 ///
                 /// @param a The alpha
-                void setAlpha(unsigned char a) { _color.a = a; }
+                constexpr void setAlpha(unsigned char a) { _color.a = a; }
 
-                /// @brief Gets the raylib Color
+                /// Gets the raylib Color
+                ///
                 /// @return The raylib Color
-                inline ::Color toRaylib() { return _color; }
+                constexpr ::Color toRaylib() { return _color; }
 
-
-
-            protected:
             private:
                 ::Color _color;
 };
