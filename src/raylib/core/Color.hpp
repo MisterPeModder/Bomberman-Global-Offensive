ca9ed8853edab479c95773d013c0e713f1cf6336
aurelenc <aurele.nicolas@epitech.eu>
2022-05-24 20:08:34 +0200

    Remove useless 'public' keyword in Color struct

--- src/raylib/core/Color.hpp
+++ src/raylib/core/Color.hpp
@@ -19,71 +19,70 @@ namespace raylib
     {
         /// The Color struct that encapsulates the raylib Color struct
         struct Color {
-            public:
-                unsigned char r;
-                unsigned char g;
-                unsigned char b;
-                unsigned char a;
+            unsigned char r;
+            unsigned char g;
+            unsigned char b;
+            unsigned char a;
 
-                /// Constructor with the red, green and blue values mandatory
-                ///
-                /// @param red the amount of red
-                /// @param green the amount of green
-                /// @param blue the amount of blue
-                /// @param alpha the alpha (defaulted to 255)
-                constexpr Color(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha = 255) : r(red), g(green), b(blue), a(alpha) {}
+            /// Constructor with the red, green and blue values mandatory
+            ///
+            /// @param red the amount of red
+            /// @param green the amount of green
+            /// @param blue the amount of blue
+            /// @param alpha the alpha (defaulted to 255)
+            constexpr Color(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha = 255) : r(red), g(green), b(blue), a(alpha) {}
 
-                /// Default constructor
-                /// Defaults everything to 255
-                constexpr Color() : r(255), g(255), b(255), a(255) {}
+            /// Default constructor
+            /// Defaults everything to 255
+            constexpr Color() : r(255), g(255), b(255), a(255) {}
 
-                /// Destructor
-                ~Color() = default;
+            /// Destructor
+            ~Color() = default;
 
-                /// Copy constructor
-                ///
-                /// @param other The color to copy
-                constexpr Color(const Color &other) : r(other.r), g(other.g), b(other.b), a(other.a) {}
+            /// Copy constructor
+            ///
+            /// @param other The color to copy
+            constexpr Color(const Color &other) : r(other.r), g(other.g), b(other.b), a(other.a) {}
 
-                /// Copy assignment operator
-                ///
-                /// @param other The color to copy
-                ///
-                /// @return The new copied color
-                constexpr Color &operator=(const Color &other)
-                {
-                    r = other.r;
-                    g = other.g;
-                    b = other.b;
-                    a = other.a;
+            /// Copy assignment operator
+            ///
+            /// @param other The color to copy
+            ///
+            /// @return The new copied color
+            constexpr Color &operator=(const Color &other)
+            {
+                r = other.r;
+                g = other.g;
+                b = other.b;
+                a = other.a;
 
-                    return *this;
-                }
+                return *this;
+            }
 
-                /// Constructor from the C Raylib Color struct
-                ///
-                /// @param other The C Raylib color to copy
-                constexpr Color(const ::Color &other) : r(other.r), g(other.g), b(other.b), a(other.a) {}
+            /// Constructor from the C Raylib Color struct
+            ///
+            /// @param other The C Raylib color to copy
+            constexpr Color(const ::Color &other) : r(other.r), g(other.g), b(other.b), a(other.a) {}
 
-                /// Copy assignment operator from a C Raylib Color struct
-                ///
-                /// @param other The C Raylib color to copy
-                ///
-                /// @return The new copied color
-                constexpr Color &operator=(const ::Color &other)
-                {
-                    r = other.r;
-                    g = other.g;
-                    b = other.b;
-                    a = other.a;
+            /// Copy assignment operator from a C Raylib Color struct
+            ///
+            /// @param other The C Raylib color to copy
+            ///
+            /// @return The new copied color
+            constexpr Color &operator=(const ::Color &other)
+            {
+                r = other.r;
+                g = other.g;
+                b = other.b;
+                a = other.a;
 
-                    return *this;
-                }
+                return *this;
+            }
 
-                /// Gets the raylib Color
-                ///
-                /// @return The raylib Color
-                constexpr ::Color asRaylib() { return {.r = r, .g = g, .b = b, .a = a}; }
+            /// Gets the raylib Color
+            ///
+            /// @return The raylib Color
+            constexpr ::Color asRaylib() { return {.r = r, .g = g, .b = b, .a = a}; }
 
         };
     }
