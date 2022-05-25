d60dd973f16c6ec34fbda9d41cc8eb413025c2a3
Yanis Guaye <yanis.guaye@epitech.eu>
2022-05-25 05:20:44 +0200

    Add dynamic bit set helper class

---
+++ src/util/BitSet.cpp
@@ -0,0 +1,148 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman: Global Offensive
+** File description:
+** A more efficient dynamic bitset
+*/
+
+#include "util/BitSet.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <ostream>
+#include <vector>
+#include <string_view>
+
+namespace util
+{
+    BitSet::BitSet(std::size_t initialSize)
+        : _size(initialSize), _store(initialSize == 0 ? 0 : std::max(std::size_t(1), initialSize >> 6), 0)
+    {
+    }
+
+    BitSet::BitSet(std::string_view bitString) : BitSet(bitString.size())
+    {
+        for (std::size_t i = 0, s = bitString.size(); i < s; ++i)
+            this->set(i, bitString[s - i - 1] == '1');
+    }
+
+    BitSet &BitSet::setAll(bool value) noexcept
+    {
+        for (auto &word : this->_store)
+            word = value ? ~std::uint64_t(0) : std::uint64_t(0);
+        this->normalize();
+        return *this;
+    }
+
+    bool BitSet::operator==(BitSet const &other) const noexcept { return this->_store == other._store; }
+
+    BitSet &BitSet::flip() noexcept
+    {
+        for (auto &word : this->_store)
+            word = ~word;
+        this->normalize();
+        return *this;
+    }
+
+    BitSet BitSet::operator~() const
+    {
+        BitSet copy(*this);
+
+        copy.flip();
+        return copy;
+    }
+
+    BitSet BitSet::operator&(BitSet const &other) const
+    {
+        BitSet result(std::min(this->_size, other._size));
+
+        for (std::size_t i = 0, s = result._store.size(); i < s; ++i)
+            result._store[i] = this->_store[i] & other._store[i];
+        result.normalize();
+        return result;
+    }
+
+    BitSet BitSet::operator|(BitSet const &other) const
+    {
+        BitSet result(std::min(this->_size, other._size));
+
+        for (std::size_t i = 0, s = result._store.size(); i < s; ++i)
+            result._store[i] = this->_store[i] | other._store[i];
+        result.normalize();
+        return result;
+    }
+
+    BitSet BitSet::operator^(BitSet const &other) const
+    {
+        BitSet result(std::min(this->_size, other._size));
+
+        for (std::size_t i = 0, s = result._store.size(); i < s; ++i)
+            result._store[i] = this->_store[i] ^ other._store[i];
+        result.normalize();
+        return result;
+    }
+
+    BitSet &BitSet::operator&=(BitSet const &other) noexcept
+    {
+        this->_resize(std::min(this->_size, other._size));
+        for (std::size_t i = 0, s = this->_store.size(); i < s; ++i)
+            this->_store[i] &= other._store[i];
+        this->normalize();
+        return *this;
+    }
+
+    BitSet &BitSet::operator|=(BitSet const &other) noexcept
+    {
+        this->_resize(std::min(this->_size, other._size));
+        for (std::size_t i = 0, s = this->_store.size(); i < s; ++i)
+            this->_store[i] |= other._store[i];
+        this->normalize();
+        return *this;
+    }
+
+    BitSet &BitSet::operator^=(BitSet const &other) noexcept
+    {
+        this->_resize(std::min(this->_size, other._size));
+        for (std::size_t i = 0, s = this->_store.size(); i < s; ++i)
+            this->_store[i] ^= other._store[i];
+        this->normalize();
+        return *this;
+    }
+
+    void BitSet::normalize() noexcept
+    {
+        if ((this->_size & 0b111111) != 0) {
+            std::uint64_t &last = *this->_store.rbegin();
+            uint64_t mask = ~((~std::uint64_t(0)) << (this->_size & 0b111111));
+            last &= mask;
+        }
+    }
+
+    void BitSet::_resize(std::size_t size)
+    {
+        this->_store.resize(size == 0 ? 0 : std::max(std::size_t(1), size >> 6));
+        this->_size = size;
+    }
+
+    BitSet &BitSet::push(bool value)
+    {
+        this->resize(this->_size + 1);
+        this->set(this->_size - 1, value);
+        return *this;
+    }
+
+    bool BitSet::pop()
+    {
+        bool value = this->test(this->_size - 1);
+        this->resize(this->_size - 1);
+        return value;
+    }
+
+    std::ostream &operator<<(std::ostream &output, BitSet const &set)
+    {
+        for (std::size_t s = set.size(), i = s; i > 0; --i)
+            output << (set.test(i - 1) ? '1' : '0');
+        return output;
+    }
+} // namespace util
---
+++ src/util/BitSet.hpp
@@ -0,0 +1,179 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman: Global Offensive
+** File description:
+** A more efficient dynamic bitset
+*/
+
+#ifndef UTIL_BIT_SET_HPP_
+#define UTIL_BIT_SET_HPP_
+
+#include <cstddef>
+#include <cstdint>
+#include <ostream>
+#include <vector>
+#include <string_view>
+
+namespace util
+{
+    /// Mimimics the API of std::bitset but with the dynamic properties of std::vector<bool>
+    class BitSet {
+      public:
+        class Reference {
+          public:
+            /// Assigns a value to the refenced bit.
+            constexpr Reference &operator=(bool value) noexcept
+            {
+                this->_set.set(this->_pos, value);
+                return *this;
+            }
+
+            /// @returns The value of the referenced bit.
+            constexpr operator bool() const noexcept { return const_cast<BitSet const &>(this->_set)[this->_pos]; }
+
+            /// @returns The inverse of the referenced bit.
+            constexpr bool operator~() const noexcept { return !bool(*this); }
+
+            /// Inverts the referenced bit.
+            constexpr Reference &flip() noexcept
+            {
+                *this = ~(*this);
+                return *this;
+            }
+
+          private:
+            BitSet &_set;
+            std::size_t _pos;
+
+            constexpr Reference(BitSet &set, std::size_t pos) : _set(set), _pos(pos) {}
+
+            friend BitSet;
+        };
+
+        /// @param initialSize An initial number of bits, all set to zero.
+        BitSet(std::size_t initialSize = 0);
+
+        /// Constructs a bitset from a strings of zeroes and ones.
+        ///
+        /// The leftmost bit is the most-significant.
+        ///
+        /// @note @b bitString must @b only contain the chars '0' and '1'.
+        BitSet(std::string_view bitString);
+
+        /// @returns The number of bits in the set.
+        constexpr std::size_t size() const noexcept { return this->_size; }
+
+        /// @note This function does not perform bounds-checking.
+        ///
+        /// @returns The value of the bit at @b pos.
+        constexpr bool test(std::size_t pos) const noexcept { return (this->_store[pos >> 6] & mask(pos)) != 0; }
+
+        /// @note This function does not perform bounds-checking.
+        ///
+        /// @returns The value of the bit at @b pos.
+        constexpr bool operator[](std::size_t pos) const noexcept { return this->test(pos); }
+
+        /// @note This function does not perform bounds-checking.
+        ///
+        /// Assigns a value to the  bit at @b pos.
+        constexpr BitSet &set(std::size_t pos, bool value = true) noexcept
+        {
+            std::uint64_t m = mask(pos);
+            if (value)
+                this->_store[pos >> 6] |= m;
+            else
+                this->_store[pos >> 6] &= ~m;
+            return *this;
+        }
+
+        /// Sets all bits to the given value.
+        BitSet &setAll(bool value = true) noexcept;
+
+        /// @note This function does not perform bounds-checking.
+        ///
+        /// @returns A reference to the bit at @b pos.
+        constexpr Reference operator[](std::size_t pos) noexcept { return Reference(*this, pos); }
+
+        /// @returns Whether the bit sets are equal, both sets must have the same size.
+        bool operator==(BitSet const &other) const noexcept;
+
+        /// @returns Whether the bit sets are not equal, both sets must have the same size.
+        bool operator!=(BitSet const &other) const noexcept = default;
+
+        /// Flips all the bits in the set.
+        ///
+        /// @returns this.
+        BitSet &flip() noexcept;
+
+        /// @returns A copy of this with all the bits flipped.
+        BitSet operator~() const;
+
+        /// Performs a bitwise AND with this set and @b other.
+        ///
+        /// @returns A new result set, its length is that of the smallest input set.
+        BitSet operator&(BitSet const &other) const;
+
+        /// Performs a bitwise OR with this set and @b other.
+        ///
+        /// @returns A new result set, its length is that of the smallest input set.
+        BitSet operator|(BitSet const &other) const;
+
+        /// Performs a bitwise XOR with this set and @b other.
+        ///
+        /// @returns A new result set, its length is that of the smallest input set.
+        BitSet operator^(BitSet const &other) const;
+
+        /// Performs a bitwise AND with this set and @b other in place.
+        ///
+        /// @returns this
+        BitSet &operator&=(BitSet const &other) noexcept;
+
+        /// Performs a bitwise OR with this set and @b other in place.
+        ///
+        /// @returns this
+        BitSet &operator|=(BitSet const &other) noexcept;
+
+        /// Performs a bitwise XOR with this set and @b other in place.
+        ///
+        /// @returns this
+        BitSet &operator^=(BitSet const &other) noexcept;
+
+        /// Changes the number of bits stored in this set.
+        ///
+        /// @param size The new number of bits, may be smaller or greater than the current size, or zero.
+        inline void resize(std::size_t size)
+        {
+            this->_resize(size);
+            this->normalize();
+        }
+
+        /// Adds the given bit to the end of the set.
+        BitSet &push(bool value);
+
+        /// Removes one bit from the end of the set.
+        ///
+        /// @note This function does not perform bounds-checking.
+        ///
+        /// @returns The value of the removed bit.
+        bool pop();
+
+      private:
+        std::size_t _size;
+        std::vector<std::uint64_t> _store;
+
+        static constexpr std::uint64_t mask(std::size_t pos) { return std::uint64_t(1) << (pos & 0b111111); }
+
+        /// Unset the trailing bits.
+        void normalize() noexcept;
+
+        /// Resize without normalizing
+        void _resize(std::size_t size);
+
+        friend Reference;
+    };
+
+    /// Prints the contents of @b set into @b output.
+    std::ostream &operator<<(std::ostream &output, BitSet const &set);
+} // namespace util
+
+#endif // !defined(UTIL_BIT_SET_HPP_)
