bbb691343fd7392af450c0c7fc54f7d9d8e93dc2
aurelenc <aurele.nicolas@epitech.eu>
2022-05-25 15:11:10 +0200

    Format 'Window' class

--- src/raylib/core/Window.cpp
+++ src/raylib/core/Window.cpp
@@ -18,45 +18,21 @@ namespace raylib
     namespace core
     {
 
-        void Window::open(int width, int height, const char *title)
-        {
-            InitWindow(width, height, title);
-        }
+        void Window::open(int width, int height, const char *title) { InitWindow(width, height, title); }
 
-        void Window::close()
-        {
-            CloseWindow();
-        }
+        void Window::close() { CloseWindow(); }
 
-        void Window::clear()
-        {
-            ClearBackground(BLACK);
-        }
+        void Window::clear() { ClearBackground(BLACK); }
 
-        bool Window::windowShouldClose()
-        {
-            return WindowShouldClose();
-        }
+        bool Window::windowShouldClose() { return WindowShouldClose(); }
 
-        void Window::setTargetFPS(int fps)
-        {
-            SetTargetFPS(fps);
-        }
+        void Window::setTargetFPS(int fps) { SetTargetFPS(fps); }
 
-        void Window::drawFPS(int x, int y)
-        {
-            DrawFPS(x, y);
-        }
+        void Window::drawFPS(int x, int y) { DrawFPS(x, y); }
 
-        void Window::beginDrawing()
-        {
-            BeginDrawing();
-        }
+        void Window::beginDrawing() { BeginDrawing(); }
 
-        void Window::endDrawing()
-        {
-            EndDrawing();
-        }
+        void Window::endDrawing() { EndDrawing(); }
 
     } // namespace core
 } // namespace raylib
