b82edfb212f30ff8da3f50098e646482e5688213
Andréas Leroux <andreas.leroux@epitech.eu>
2022-05-25 12:25:11 +0200

    Add Map documentation
    Linked: #11

--- src/bomberman/map/Map.hpp
+++ src/bomberman/map/Map.hpp
@@ -15,28 +15,60 @@ namespace bomberman
 {
     namespace map
     {
+        /// Bomberman Map
         class Map {
           public:
+            /// Map element
             enum class Element {
-                Wall,
-                Crate,
-                Empty,
+                Wall,  /// Indestructible wall
+                Crate, /// Destructible crate
+                Empty, /// Nothing
 
             };
 
+            /// Construct a new Map
+            ///
+            /// @param fillPercent initial map cell fill, may change due to cellular automata.
+            /// @param width width of the map
+            /// @param height height of the map
             Map(size_t fillPercent = 40, size_t width = 13, size_t height = 13);
+
+            /// Destroy the map
             ~Map() = default;
 
+            /// Generate the map.
+            ///
+            /// @param fillPercent initial map cell fill, may change due to cellular automata.
+            /// @param width width of the map
+            /// @param height height of the map
             void generate(size_t fillPercent, size_t width, size_t height);
-            void debugDisplay(std::ostream &stream) const;
 
+            /// Get the Element at the position (x, y)
+            ///
+            /// @param x x position
+            /// @param y y position
+            /// @return Element element at the position (x, y)
             Element getElement(size_t x, size_t y) const;
+
+            /// Get the Element at the position (x, y)
+            ///
+            /// @param x x position
+            /// @param y y position
+            /// @return Element& element at the position (x, y)
             Element &getElement(size_t x, size_t y);
 
+            /// Get the Width of the map
+            ///
+            /// @return size_t width
             size_t getWidth() const;
+
+            /// Get the height of the map
+            ///
+            /// @return size_t height
             size_t getHeight() const;
 
           private:
+            /// Ensure the corners to be empty to avoid invalid player spawn
             void freeCorners();
 
             size_t _width;
