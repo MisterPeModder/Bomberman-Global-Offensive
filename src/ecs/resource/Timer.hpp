e6f87bd79a38257dd020edd6557d9b49acad5576
Yanis Guaye <yanis.guaye@epitech.eu>
2022-05-28 18:52:33 +0200

    Add Timer ECS world resource

---
+++ src/ecs/resource/Timer.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman: Global Offensive
+** File description:
+** ECS - Timer World Resource
+*/
+
+#include "ecs/resource/Resource.hpp"
+
+#include <chrono>
+
+/// Entity-Component-System namespace.
+namespace ecs
+{
+    /// Simple timer resource.
+    class Timer : public Resource {
+      public:
+        /// Creates a new timer.
+        explicit Timer() noexcept;
+
+        /// Resets the timer.
+        void reset() noexcept;
+
+        /// @returns The seconds since the last call to reset() (or construction of the Timer).
+        double elapsed() const noexcept;
+
+      private:
+        std::chrono::steady_clock::time_point _lastReset;
+    };
+} // namespace ecs
