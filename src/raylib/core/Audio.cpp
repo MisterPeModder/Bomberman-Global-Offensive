c63a1c1a009205d5c5a21204de52bd20cd1da7c9
François <francois.baudouin@epitech.eu>
2022-05-30 10:10:20 +0200

    add management audio Loading & unloading are now aviable, and we can know if the audio device is ready

--- src/raylib/core/Audio.cpp
+++ src/raylib/core/Audio.cpp
@@ -14,3 +14,18 @@ raylib::core::Audio::Audio()
 raylib::core::Audio::Audio::~Audio()
 {
 }
+
+namespace raylib {
+    namespace core {
+        Sound Audio::loadSound(const std::string fileName) { return LoadSound(fileName.c_str());}
+
+        void Audio::unloadWave(Wave wave) {return UnloadWave(wave);}
+
+        void Audio::setMasterVolume(float volume) {return setMasterVolume(volume);}
+
+        bool Audio::isAudioDeviceReady(void) {return IsAudioDeviceReady();}
+
+    }
+}
+
+
--- src/raylib/core/Audio.hpp
+++ src/raylib/core/Audio.hpp
@@ -9,6 +9,7 @@
 #define RAYLIB_CORE_AUDIO_HPP_
 
 #include <raylib.h>
+#include <string>
 
 namespace raylib
 {
@@ -17,6 +18,19 @@ namespace raylib
             public:
                 Audio();
                 ~Audio();
+                //masterisation of music
+                Sound loadSound(const std::string fileName);
+                void unloadWave(Wave wave);
+                void setMasterVolume(float volume);
+                bool isAudioDeviceReady(void);
+                void closeAudioDevice(void);
+                void initAudioDevice(void);
+                void playSound(Sound sound);
+                void stopSound(Sound sound);
+                void pauseSound(Sound sound);
+                void resumeSound(Sound sound);
+                bool isSoundPlaying(Sound sound);
+                void setSoundVolume(Sound sound, float volume);
 
             protected:
             private:
