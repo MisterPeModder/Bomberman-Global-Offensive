23122dc5ce9188c856db797d0a4db05d78b15acc
Andréas Leroux <andreas.leroux@epitech.eu>
2022-05-31 21:51:19 +0200

    Add Keybinds and GamepadInput related documentation
    Linked: #66

--- src/game/settings/GamepadInput.hpp
+++ src/game/settings/GamepadInput.hpp
@@ -14,23 +14,76 @@ namespace game
 {
     namespace settings
     {
+        /// Define a gamepad input. An input can be a button or a specific axis direction (Ex: the joystick left
+        /// movement).
         class GamepadInput {
           public:
+            /// Used to separate an axis in two inputs. Needed for movements (left/right).
             enum class AxisDirection {
-                None,
-                Negative,
-                Positive,
+                None,     //< Invalid
+                Negative, //< Negative value ([-1, 0])
+                Positive, //< Positive range ([0, 1])
             };
 
+            /// Construct a new GamepadInput from a simple button.
+            ///
+            /// @param button button to check.
             GamepadInput(raylib::core::Gamepad::Button button);
+
+            /// Construct a new GamepadInput from an axis direction.
+            ///
+            /// @param axis axis to check.
+            /// @param direction direction to check.
             GamepadInput(raylib::core::Gamepad::Axis axis, AxisDirection direction);
+
+            /// Construct a copy of a GamepadInput object.
+            ///
+            /// @param other object to copy.
+            ///
+            GamepadInput(const GamepadInput &other) = default;
+
+            /// Destroys the GamepadInput.
             ~GamepadInput() = default;
 
+            /// Copy a GamepadInput object.
+            ///
+            /// @param other object to copy.
+            /// @return GamepadInput& @b this.
+            ///
+            GamepadInput &operator=(const GamepadInput &other) = default;
+
+            /// Test if the input stored is a button.
+            ///
+            /// @retval true when the input stored is a valid button.
+            /// @retval false when the input stored is an axis direction.
             bool isButton() const;
 
+            /// Convert the input to string for settings saving in files.
+            ///
+            /// @note The format for button inputs are "[button]" where [button] is replaced by the integral value of
+            /// the Gamepad::Button enum.
+            /// @note The format for axis direction are "[axis]:[direction]" where [axis] is replaced by the integral
+            /// value of the Gamepad::Axis enum and [direction] is replaced with the integral value of the AxisDirection
+            /// enum. value
+            ///
+            /// @return std::string string representation of @b this.
             std::string toString() const;
 
+            /// Lower than comparison operator overload.
+            ///
+            /// @note This overload is required to store @ref GamepadInput objects as key in @ref std::map.
+            /// @note It doesn't make sense to test if an input is lower than an other except for sorting.
+            ///
+            /// @param other object to compare with.
+            /// @retval true If @b this is considered lower than @c other.
+            /// @retval false Otherwise.
             bool operator<(const GamepadInput &other) const;
+
+            /// Equality comparison operator overload.
+            ///
+            /// @param other object to compare with.
+            /// @retval true If @b this and @c other are equals.
+            /// @retval false otherwise.
             bool operator==(const GamepadInput &other) const;
 
           private:
--- src/game/settings/Keybinds.hpp
+++ src/game/settings/Keybinds.hpp
@@ -18,21 +18,77 @@ namespace game
 {
     namespace settings
     {
+        /// Bindings of keyboards/gamepad inputs to game actions.
         class Keybinds {
           public:
+            /// Construct a default keybinds set.
+            /// @note Will call @ref loadDefaults().
             Keybinds();
+
+            /// Construct a copy of a Keybinds object
+            ///
+            /// @param other keybinds to copy.
+            Keybinds(const Keybinds &other) = default;
+
+            /// Destroy a keybinds object.
             ~Keybinds() = default;
 
+            /// Copy a Keybinds object.
+            ///
+            /// @param other keybinds to copy.
+            /// @return Keybinds& @b this.
+            Keybinds &operator=(const Keybinds &other) = default;
+
+            /// Load the game default keybinds.
+            /// @todo Add some documentation for the default keybinds and link it here.
             void loadDefaults();
 
+            /// Bind a keyboard key to a game action.
+            /// @note Multiple key can be bound to the same action.
+            /// @note Binding a key that was previously bind to an action will override the bind.
+            ///
+            /// @param key key.
+            /// @param action game action.
             void setKeyboardBinding(raylib::core::Keyboard::Key key, GameAction action);
+
+            /// Unbind a key from its game action.
+            /// @note Does nothing if the key is already unbound.
+            ///
+            /// @param key key to unbind.
             void unbindKey(raylib::core::Keyboard::Key key);
+
+            /// Bind a gamepad input to a game action.
+            /// @note Multiple inputs can be bound to the same action.
+            /// @note Binding a gamepad input that was previously bind to an action will override the bind.
+            ///
+            /// @param gamepadInput input.
+            /// @param action game action.
             void setGamepadBinding(const GamepadInput &gamepadInput, GameAction action);
+
+            /// Unbind a gamepad input from its game action.
+            /// @note Does nothing if the gamepad input is already unbound.
+            ///
+            /// @param gamepadInput gamepad input to unbind.
             void unbindGamepadInput(const GamepadInput &gamepadInput);
 
+            /// Get the map of the keyboard bindings.
+            ///
+            /// @return const std::map<raylib::core::Keyboard::Key, GameAction>& bindings map.
+            ///
             const std::map<raylib::core::Keyboard::Key, GameAction> &getKeyboardBindings() const;
+
+            /// Get the map of the gamepad bindings.
+            ///
+            /// @return const std::map<GamepadInput, GameAction>& bindings map.
+            ///
             const std::map<GamepadInput, GameAction> &getGamepadBindings() const;
 
+            /// Output the bindings in the @c stream.
+            /// @note This is used to save bindings in files between sessions.
+            ///
+            /// @param stream output stream.
+            /// @return const Keybinds& @b this.
+            ///
             const Keybinds &operator>>(std::ostream &stream) const;
 
           private:
@@ -42,6 +98,12 @@ namespace game
     } // namespace settings
 } // namespace game
 
+/// Output stream operator overload.
+/// @note This function is used for the keybinds save in the file.
+///
+/// @param stream output stream.
+/// @param keybinds keybinds to export.
+/// @return std::ostream& output stream.
 std::ostream &operator<<(std::ostream &stream, const game::settings::Keybinds keybinds);
 
 #endif /* !GAME_SETTINGS_KEYBINDS_HPP_ */
