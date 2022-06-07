daab3e334bfae89df29815045c5500efe17c6f81
François <francois.baudouin@epitech.eu>
2022-06-07 16:39:48 +0200

    Initialization of MainMenu

---
+++ src/menu/MainMenu.cpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman-Global-Offensive
+** File description:
+** Menu
+*/
+
+#include "MainMenu.hpp"
+
+namespace Menu
+{
+    MainMenu::MainMenu()
+    {
+    }
+
+    MainMenu::~MainMenu()
+    {
+    }
+    
+}
---
+++ src/menu/MainMenu.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman-Global-Offensive
+** File description:
+** Menu
+*/
+
+#pragma once
+
+#include "ecs/Component.hpp"
+#include "ecs/Storage.hpp"
+#include "ecs/System.hpp"
+#include "ecs/World.hpp"
+#include "ecs/join.hpp"
+#include "ecs/resource/Timer.hpp"
+
+namespace Menu
+{
+    class MainMenu {
+      public:
+        MainMenu();
+        ~MainMenu();
+
+      protected:
+      private:
+    };
+} // namespace Menu
