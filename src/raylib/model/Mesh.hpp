1268b75752270d1dfa8182ab494de15acb392b20
aurelenc <aurele.nicolas@epitech.eu>
2022-06-09 15:40:14 +0200

    Fix mutable/immutable keyword in documentation

--- src/raylib/model/Mesh.hpp
+++ src/raylib/model/Mesh.hpp
@@ -15,77 +15,77 @@ extern "C"
 #include "raylib/core/Vector3.hpp"
 #include "raylib/textures/Image.hpp"
 
-namespace raylib
+namespace raylib::model
 {
-    namespace model
-    {
-        /// Mesh class
-        class Mesh {
-          public:
-            /// Deleted default constructor
-            Mesh() = delete;
+    /// Mesh class
+    class Mesh {
+      public:
+        /// Deleted default constructor
+        Mesh() = delete;
 
-            /// Deleted copy operator
-            ///
-            /// @param other the mesh to not copy
-            Mesh &operator=(const Mesh &other) = delete;
+        /// Deleted copy operator
+        Mesh &operator=(const Mesh &other) = delete;
 
-            /// Mesh copy constructor
-            ///
-            /// @param other the mesh to copy
-            Mesh(const Mesh &other);
+        /// Mesh copy constructor
+        ///
+        /// @param other the mesh to copy
+        Mesh(const Mesh &other);
 
-            /// C Raylib mesh import constructor
-            ///
-            /// @param mesh the C Raylib mesh to import
-            Mesh(const ::Mesh &mesh);
+        /// C Raylib mesh import constructor
+        ///
+        /// @param mesh the C Raylib mesh to import
+        Mesh(const ::Mesh &mesh);
 
-            /// Destructor
-            ~Mesh();
+        /// Destructor
+        ~Mesh();
 
-            /// Generates a cubic mesh
-            ///
-            /// @param width the width of the cube
-            /// @param height the height of the cube
-            /// @param length the length of the cube
-            ///
-            /// @return The created cubic mesh
-            static Mesh genCube(float width, float height, float length);
+        /// Generates a cubic mesh
+        ///
+        /// @param width the width of the cube
+        /// @param height the height of the cube
+        /// @param length the length of the cube
+        ///
+        /// @return The created cubic mesh
+        static Mesh genCube(float width, float height, float length);
 
-            /// Generates a cubic mesh
-            ///
-            /// @param size the size of the cube
-            ///
-            /// @return The created cubic mesh
-            static Mesh genCube(const raylib::core::Vector3 &size);
+        /// Generates a cubic mesh
+        ///
+        /// @param size the size of the cube
+        ///
+        /// @return The created cubic mesh
+        static Mesh genCube(const raylib::core::Vector3 &size);
 
-            /// Generates a cubic map mesh from an image
-            ///
-            /// @param image the image used to generate the mesh
-            /// @param width the width of the cube
-            /// @param height the height of the cube
-            /// @param length the length of the cube
-            ///
-            /// @return The created cubic mesh
-            static Mesh genCubicMap(const raylib::textures::Image &image, float width, float height, float length);
+        /// Generates a cubic map mesh from an image
+        ///
+        /// @param image the image used to generate the mesh
+        /// @param width the width of the cube
+        /// @param height the height of the cube
+        /// @param length the length of the cube
+        ///
+        /// @return The created cubic mesh
+        static Mesh genCubicMap(const raylib::textures::Image &image, float width, float height, float length);
 
-            /// Generates a cubic map mesh from an image
-            ///
-            /// @param image the image used to generate the mesh
-            /// @param size the size of the cube
-            ///
-            /// @return The created cubic mesh
-            static Mesh genCubicMap(const raylib::textures::Image &image, const raylib::core::Vector3 &vector);
+        /// Generates a cubic map mesh from an image
+        ///
+        /// @param image the image used to generate the mesh
+        /// @param size the size of the cube
+        ///
+        /// @return The created cubic mesh
+        static Mesh genCubicMap(const raylib::textures::Image &image, const raylib::core::Vector3 &vector);
 
-            /// Gets the C Raylib mutable version of the mesh
-            ///
-            /// @return The raylib version of the mesh
-            ::Mesh const &asRaylib() const;
+        /// Gets the C Raylib immutable version of the mesh
+        ///
+        /// @return The raylib version of the mesh
+        ::Mesh const &asRaylib() const;
 
-          private:
-            ::Mesh _mesh;
-        };
-    } // namespace model
-} // namespace raylib
+        /// Gets the C Raylib mutable version of the mesh
+        ///
+        /// @return The raylib version of the mesh
+        ::Mesh &asRaylib();
+
+      private:
+        ::Mesh _mesh;
+    };
+} // namespace raylib::model
 
 #endif /* !RAYLIB_CORE_MESH_HPP_ */
