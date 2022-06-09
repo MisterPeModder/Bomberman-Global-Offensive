404a90ee574008aff902e3d2787aaeb8cc018abc
Yanis Guaye <yanis.guaye@epitech.eu>
2022-06-09 17:04:16 +0200

    Fix missing trailing ECS entities when using optional joins
    Fixes #106

--- src/ecs/join/Joinable.hpp
+++ src/ecs/join/Joinable.hpp
@@ -25,13 +25,19 @@ namespace ecs
         // empty, only specializations of JoinTraits contain data here.
     };
 
+    /// Optional Join traits.
+    template <typename J> struct JoinTraitsExt {
+        constexpr static void adjustMask([[maybe_unused]] J &joinable, [[maybe_unused]] std::size_t maxSize) {}
+    };
+
     /// Implemented by types that can be joined.
     template <typename J>
-    concept Joinable = requires(J &joinable, J const &cjoinable, std::size_t index)
+    concept Joinable = requires(J &joinable, J const &cjoinable, std::size_t index, std::size_t maxSize)
     {
         typename JoinTraits<J>::Data;
 
         // clang-format off
+        { JoinTraitsExt<J>::adjustMask(joinable, maxSize) } -> std::same_as<void> ;
         { JoinTraits<J>::getMask(cjoinable) } -> std::same_as<util::BitSet const &>;
         { JoinTraits<J>::getData(joinable, index) } -> std::same_as<typename JoinTraits<J>::Data>;
         // clang-format on
