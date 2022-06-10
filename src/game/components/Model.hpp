d668938f14d08489b97b16cd097f2d446c0f672d
aurelenc <aurele.nicolas@epitech.eu>
2022-06-10 23:36:40 +0200

    Add model component constructor from mesh

--- src/game/components/Model.hpp
+++ src/game/components/Model.hpp
@@ -4,11 +4,13 @@
 #include <filesystem>
 #include "ecs/Component.hpp"
 #include "raylib/model/Model.hpp"
+// #include "raylib/model/Mesh.hpp"
 
 namespace game
 {
     struct Model : public ecs::Component, public raylib::model::Model {
         Model(raylib::model::Model &pmodel) : raylib::model::Model(pmodel) {}
+        // Model(const raylib::model::Mesh &pmesh) : raylib::model::Model(pmesh) {}
         Model(std::filesystem::path &path) : raylib::model::Model(path) {}
     };
 } // namespace game
