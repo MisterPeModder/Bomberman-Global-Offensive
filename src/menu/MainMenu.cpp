cc33614b5d158a41215c81eccc9d9d790445fdd3
François <francois.baudouin@epitech.eu>
2022-06-10 17:17:32 +0200

    add button play and start placi ng logo a compenent to create/place image is currently develop, need to fix technical issu with it

--- src/menu/MainMenu.cpp
+++ src/menu/MainMenu.cpp
@@ -14,36 +14,42 @@
 #include "game/systems/DrawText.hpp"
 #include "game/systems/InputManager.hpp"
 #include "logger/Logger.hpp"
+#include "raylib/textures/Image.hpp"
 
 namespace Menu
 {
-    MainMenu::MainMenu()
-    {
-        menu.addResource<game::Users>();
-        menu.addSystem<game::DrawText>();
-        menu.addSystem<game::InputManager>();
-    }
+    MainMenu::MainMenu() {}
     MainMenu::~MainMenu() {}
 
-    void MainMenu::createsButtons()
+    void MainMenu::createsButtons(ecs::World &world)
     {
-        game::Rectangle test({50, 50}, {1, 1}, raylib::core::Color::RED);
-        menu.addEntity()
-            .with<game::Position>(50.f, 50.f)
+       /*  struct Image : public ecs::Component, public raylib::textures::Image {
+            Image(const std::filesystem::path &fileName, raylib::core::Vector2 position = {},
+                const raylib::core::Color &color = raylib::core::Color::DEFAULT_COLOR)
+                : raylib::textures::Image(fileName)
+            {
+            }
+        };
+        world.addEntity()
+            .with<game::Position>(0.f, 0.f)
+            .with<game::Color>
+            .with<Image>("assets/logo.jpg")
+            .build(); */
+
+        world.addEntity()
+            .with<game::Position>(0.f, 0.f)
             .with<game::Textual>("PLAY", 20, raylib::core::Color::WHITE)
             .with<game::Controlable>(game::User::UserId::User1,
-            [](ecs::Entity self, ecs::SystemData data, const game::Users::ActionEvent &event) {
-                (void)self;
-                (void)data;
-                (void)event;
-                Logger::logger.log(Logger::Severity::Debug, [&](std::ostream &writer) {
-                    writer << "PLAYED! " << event.value << ", " << static_cast<size_t>(event.action);
-                });
-                return false;
-            })
+                [](ecs::Entity self, ecs::SystemData data, const game::Users::ActionEvent &event) {
+                    (void)self;
+                    (void)data;
+                    (void)event;
+                    Logger::logger.log(Logger::Severity::Debug, [&](std::ostream &writer) {
+                        writer << "PLAYED! " << event.value << ", " << static_cast<size_t>(event.action);
+                    });
+                    return false;
+                })
             .build();
     }
-
-    void MainMenu::run() { menu.runSystems(); }
 
 } // namespace Menu
