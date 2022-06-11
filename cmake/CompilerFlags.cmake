0160c01e062638405de178e77c1712381d79fb22
Yanis Guaye <yanis.guaye@epitech.eu>
2022-06-11 18:42:29 +0200

    Fix access to invalid stack memory on the main loop

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -35,8 +35,14 @@ elseif (COMPILER_TYPE MATCHES "clang")
     if (EMSCRIPTEN)
         message(STATUS "Enabling Emscripten-specific options")
 
-        add_compile_options(-fexceptions -sDISABLE_EXCEPTION_CATCHING=0)
-        add_link_options(-fexceptions -sDISABLE_EXCEPTION_CATCHING=0 -sUSE_GLFW=3 -sASSERTIONS=1 -sWASM=1 -sEXPORTED_RUNTIME_METHODS=ccall,cwrap -sALLOW_TABLE_GROWTH)
+        add_compile_options(
+            -fexceptions -sDISABLE_EXCEPTION_CATCHING=0
+            "$<$<CONFIG:DEBUG>:-gsource-map>"
+        )
+        add_link_options(
+            -fexceptions -sDISABLE_EXCEPTION_CATCHING=0 -sUSE_GLFW=3 -sASSERTIONS=1 -sWASM=1 -sEXPORTED_RUNTIME_METHODS=ccall,cwrap -sALLOW_TABLE_GROWTH
+            "$<$<CONFIG:DEBUG>:-gsource-map>"
+        )
         set(CMAKE_EXECUTABLE_SUFFIX ".html") # Build using a HTML shell template
     endif()
 endif()
