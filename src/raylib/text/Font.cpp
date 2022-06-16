7b1aebf3774171510f00d3d4f5ac86a6e90cf007
Yanis Guaye <yanis.guaye@epitech.eu>
2022-06-16 11:18:03 +0200

    Raylib font encapsulation

---
+++ src/raylib/text/Font.cpp
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman: Global Offensive
+** File description:
+** Raylib text module: Font
+*/
+
+#include "Font.hpp"
+
+extern "C"
+{
+#include <raylib.h>
+}
+
+#include <filesystem>
+#include <span>
+#include <utility>
+#include <string_view>
+
+namespace raylib::text
+{
+    Font::Font() noexcept : _initialized(false) {}
+
+    Font::Font(::Font &&raylibFont) noexcept : _initialized(true), _font(std::move(raylibFont)) {}
+
+    Font::Font(std::filesystem::path const &path) : _initialized(true), _font(::LoadFont(path.generic_string().c_str()))
+    {
+    }
+
+    Font::Font(std::filesystem::path const &path, int size, int *chars, int glyphCount)
+        : _initialized(true), _font(::LoadFontEx(path.generic_string().c_str(), size, chars, glyphCount))
+    {
+    }
+
+    Font::Font(std::string_view fileType, std::span<unsigned char const> data, int size, int *chars, int glyphCount)
+        : _initialized(true),
+          _font(::LoadFontFromMemory(fileType.data(), data.data(), data.size_bytes(), size, chars, glyphCount))
+    {
+    }
+
+    Font::Font(Font &&other) : _initialized(other._initialized), _font(std::move(other._font))
+    {
+        other._initialized = false;
+    }
+
+    Font::~Font()
+    {
+        if (this->_initialized) {
+            UnloadFont(this->_font);
+            this->_initialized = false;
+        }
+    }
+
+    Font Font::getDefault() noexcept { return Font(::GetFontDefault()); }
+
+} // namespace raylib::text
---
+++ src/raylib/text/Font.hpp
@@ -0,0 +1,79 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman: Global Offensive
+** File description:
+** Raylib text module: Font
+*/
+
+#ifndef RAYLIB_TEXT_FONT_HPP_
+#define RAYLIB_TEXT_FONT_HPP_
+
+extern "C"
+{
+#include <raylib.h>
+}
+
+#include <filesystem>
+#include <span>
+#include <string_view>
+
+/// Raylib text module.
+namespace raylib::text
+{
+    class Font {
+      public:
+        /// Default (uninitialized) constructor.
+        Font() noexcept;
+
+        /// Construct a Font from a raylib font structure.
+        ///
+        /// @param raylibFont Raw raylib font.
+        Font(::Font &&raylibFont) noexcept;
+
+        /// Loads a font from the given path.
+        ///
+        /// @param path The font file path.
+        Font(std::filesystem::path const &path);
+
+        /// Loads a font from the given path.
+        ///
+        /// @param path The font file path.
+        /// @param size The font's size.
+        /// @param chars The Unicode codepoints supported by the font, may be @b nullptr.
+        /// @param glyphCount The number of glyphs in the font.
+        Font(std::filesystem::path const &path, int size, int *chars, int glyphCount);
+
+        /// Loads a font from memory.
+        ///
+        /// @param fileType The file type, only ".ttf" and ".otf" are supported.
+        /// @param data The font's raw data.
+        /// @param size The font's size.
+        /// @param chars The Unicode codepoints supported by the font, may be @b nullptr.
+        /// @param glyphCount The number of glyphs in the font.
+        Font(std::string_view fileType, std::span<unsigned char const> data, int size, int *chars, int glyphCount);
+
+        /// Cannot copy fonts.
+        Font(Font const &) = delete;
+
+        /// Move constructor.
+        Font(Font &&);
+
+        /// Destructor.
+        ~Font();
+
+        /// @returns The default font instance.
+        static Font getDefault() noexcept;
+
+        /// @returns A constant reference to the inner raylib font object.
+        constexpr ::Font const &asRaylib() const noexcept { return this->_font; }
+
+        /// @returns A mutable reference to the inner raylib font object.
+        constexpr ::Font &asRaylib() noexcept { return this->_font; }
+
+      private:
+        bool _initialized;
+        ::Font _font;
+    };
+} // namespace raylib::text
+
+#endif // !defined(RAYLIB_TEXT_FONT_HPP_)
