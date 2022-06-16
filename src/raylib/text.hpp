c178c2b1403aea5106e4d5d76bae2d5248357b11
Yanis Guaye <yanis.guaye@epitech.eu>
2022-06-16 04:22:11 +0200

    Add encapsulation for raylib `GetCodepoint`

--- src/raylib/text.hpp
+++ src/raylib/text.hpp
@@ -13,13 +13,22 @@
 /// Raylib text module.
 namespace raylib::text
 {
-    /// Appends an Unicode codepoint to the end of @b str.
+    /// Appends an UTF-8 codepoint to the end of @b str.
     ///
     /// @note This function is not safe to call over multiple threads.
     ///
     /// @param str The string to modify.
     /// @param codepoint An Unicode codepoint.
-    void pushUnicodeCodepoint(std::string &str, int codepoint);
+    void pushUtf8Codepoint(std::string &str, int codepoint) noexcept;
+
+    /// Removes the last UTF-8 codepoint from the end of @b str.
+    ///
+    /// @note This function is not safe to call over multiple threads.
+    ///
+    /// @param str The string to modify.
+    ///
+    /// @return The removed codepoint, or 0 if @b str was empty or not valid UTF-8.
+    int popUtf8Codepoint(std::string &str) noexcept;
 } // namespace raylib::text
 
 #endif // !defined(RAYLIB_TEXT_HPP_)
--- tests/raylib/test_text.cpp
+++ tests/raylib/test_text.cpp
@@ -12,35 +12,78 @@
 
 using namespace raylib::text;
 
-TEST(pushUnicodeCodepoint, asciiChars)
+TEST(pushUtf8Codepoint, asciiChars)
 {
     std::string buf;
 
-    pushUnicodeCodepoint(buf, static_cast<int>('T'));
-    pushUnicodeCodepoint(buf, static_cast<int>('e'));
-    pushUnicodeCodepoint(buf, static_cast<int>('s'));
-    pushUnicodeCodepoint(buf, static_cast<int>('t'));
+    pushUtf8Codepoint(buf, static_cast<int>('T'));
+    pushUtf8Codepoint(buf, static_cast<int>('e'));
+    pushUtf8Codepoint(buf, static_cast<int>('s'));
+    pushUtf8Codepoint(buf, static_cast<int>('t'));
 
     ASSERT_EQ(buf, "Test");
 }
 
-TEST(pushUnicodeCodepoint, latinChars)
+TEST(pushUtf8Codepoint, latinChars)
 {
     std::string buf;
 
-    pushUnicodeCodepoint(buf, 0xE0);
-    pushUnicodeCodepoint(buf, 0xE8);
-    pushUnicodeCodepoint(buf, 0xAE);
+    pushUtf8Codepoint(buf, 0xE0);
+    pushUtf8Codepoint(buf, 0xE8);
+    pushUtf8Codepoint(buf, 0xAE);
 
     ASSERT_EQ(buf, "àè®");
 }
 
-TEST(pushUnicodeCodepoint, emoji)
+TEST(pushUtf8Codepoint, emoji)
 {
     std::string buf;
 
-    pushUnicodeCodepoint(buf, 0x1F346);
-    pushUnicodeCodepoint(buf, 0x1F4A6);
+    pushUtf8Codepoint(buf, 0x1F346);
+    pushUtf8Codepoint(buf, 0x1F4A6);
 
     ASSERT_EQ(buf, "🍆💦");
+}
+
+TEST(popUtf8Codepoint, asciiChars)
+{
+    std::string buf = "Test";
+
+    EXPECT_EQ(popUtf8Codepoint(buf), static_cast<int>('t'));
+    EXPECT_EQ(buf.size(), 3);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), static_cast<int>('s'));
+    EXPECT_EQ(buf.size(), 2);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), static_cast<int>('e'));
+    EXPECT_EQ(buf.size(), 1);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), static_cast<int>('T'));
+    EXPECT_EQ(buf.size(), 0);
+}
+
+TEST(popUtf8Codepoint, hybrid)
+{
+    std::string buf = "ᚦéjà?🤔";
+
+    EXPECT_EQ(popUtf8Codepoint(buf), 0x1F914);
+    EXPECT_EQ(buf.size(), 9);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), static_cast<int>('?'));
+    EXPECT_EQ(buf.size(), 8);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), 0xE0);
+    EXPECT_EQ(buf.size(), 6);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), static_cast<int>('j'));
+    EXPECT_EQ(buf.size(), 5);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), 0xE9);
+    EXPECT_EQ(buf.size(), 3);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), 0x16A6);
+    EXPECT_EQ(buf.size(), 0);
+
+    EXPECT_EQ(popUtf8Codepoint(buf), 0);
+    EXPECT_EQ(buf.size(), 0);
 }
