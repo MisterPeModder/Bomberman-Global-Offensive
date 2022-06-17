70933f92e1d5e52ad3be67e742cc7005cc2ed183
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-06-17 15:44:35 +0200

    Resize the model

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -215,7 +215,7 @@ namespace game
                 .with<components::Living>(_params.livesCount)
                 .with<components::Collidable>()
                 .with<components::Player>()
-                .with<components::Size>(1.f, 1.f, 1.f)
+                .with<components::Size>(0.5f, 0.5, 0.5f)
                 .with<components::ModelReference>(_world.getResource<resources::Models>().get("player"))
                 .with<components::Color>(raylib::core::Color::WHITE)
                 .with<components::RotationAngle>(90.0f)
