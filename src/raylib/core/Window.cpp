e3cc168042161f802dae7c0a7203154ee8a1ffa6
Yanis Guaye <yanis.guaye@epitech.eu>
2022-06-17 06:27:18 +0200

    Fix crash when trying to paste empty clipboard

--- src/raylib/core/Window.cpp
+++ src/raylib/core/Window.cpp
@@ -55,7 +55,14 @@ namespace raylib
 
         void Window::setIcon(textures::Image const &icon) { SetWindowIcon(icon.asRaylib()); }
 
-        std::string_view Window::getClipboard() { return ::GetClipboardText(); }
+        std::string_view Window::getClipboard()
+        {
+            char const *text = ::GetClipboardText();
+
+            if (text == nullptr)
+                return {};
+            return text;
+        }
 
         void Window::setClipboard(std::string_view text) { ::SetClipboardText(text.data()); }
 
