0cb76e0d15706d968c0d1bbbe0119041f230a2b4
François <francois.baudouin@epitech.eu>
2022-06-17 16:17:30 +0200

    obsolete file

--- src/game/scenes/MainMenuScene.cpp
+++ src/game/scenes/MainMenuScene.cpp
@@ -53,7 +53,7 @@
 
 static void loadMainMenuScene(ecs::World &world)
 {
-    std::filesystem::path logoPath = util::makePath("assets", "icon.png");
+    static const std::filesystem::path logoPath = util::makePath("assets", "icon.png");
     raylib::textures::Image image(logoPath, {1, 2}, raylib::core::Color::WHITE);
 
     world.addEntity()
