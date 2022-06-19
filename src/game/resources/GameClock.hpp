c33b587af35f6799663ec9f5111cee1c7ff3ab3c
Andréas Leroux <andreas.leroux@epitech.eu>
2022-06-19 22:40:15 +0200

    Add missing GameClock documentation

--- src/game/resources/GameClock.hpp
+++ src/game/resources/GameClock.hpp
@@ -14,19 +14,35 @@
 
 namespace game::resources
 {
+    /// Game related clock.
     class GameClock : public ecs::Resource {
       public:
+        /// Starts the clock.
         GameClock();
 
+        /// Get the effective time passed in the game.
         std::chrono::milliseconds getTime() const;
 
+        /// Get the time elapsed since the last frame.
         std::chrono::milliseconds elapsed() const;
+        /// Get the time elapsed since the last frame (in seconds).
         float elapsedSeconds() const;
 
+        /// Update the internal time. (must be call one time per frame).
         void update();
+
+        /// (Un)pause the clock. When the clock is paused, time stops: elapsed returns 0 and the internal time doesn't
+        /// change.
+        /// @param pause If the clock must be paused.
         void pause(bool pause = true);
+
+        /// Toggle the pause
         void togglePause();
 
+        /// Check if the clock is paused.
+        ///
+        /// @retval true if the clock is paused.
+        /// @retval false if the clock is running.
         bool isPaused() const;
 
       private:
